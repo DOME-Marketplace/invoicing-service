6b438cd19fb434c5d2ce7c5a6ff921ad31988b06
Paolo Fabriani <paolo.fabriani@eng.it>
2025-11-10 19:17:14 +0100

    related parties retrieved from ACBR and not from Product

--- src/main/java/it/eng/dome/invoicing/engine/controller/CalculateTaxesController.java
+++ src/main/java/it/eng/dome/invoicing/engine/controller/CalculateTaxesController.java
@@ -51,8 +51,8 @@ public class CalculateTaxesController {
    		try {
 
    			// 1) retrieve the Product, the CustomerBill and the AppliedCustomerBillingRate list from the ApplyTaxesRequestDTO
-   			Product product = dto.getProduct();
-   			Assert.state(!Objects.isNull(product), "Missing the instance of Product in the ApplyTaxesRequestDTO");
+//   			Product product = dto.getProduct();
+//   			Assert.state(!Objects.isNull(product), "Missing the instance of Product in the ApplyTaxesRequestDTO");
 
    			CustomerBill cb=dto.getCustomerBill();
    			Assert.state(!Objects.isNull(cb), "Missing the CustomerBill in the ApplyTaxesRequestDTO");
@@ -62,7 +62,7 @@ public class CalculateTaxesController {
    			
    	        // 2) calculate the taxes
    			//ApplyTaxesResponseDTO billsWithTaxes = taxService.applyTaxes(product, cb, bills);
-   			BillingResponseDTO billsWithTaxes=taxService.applyTaxes(product, cb, bills);
+   			BillingResponseDTO billsWithTaxes=taxService.applyTaxes(cb, bills);
             return ResponseEntity.ok(billsWithTaxes);
    		}
    		catch(Exception e) {
--- src/main/java/it/eng/dome/invoicing/engine/service/TaxService.java
+++ src/main/java/it/eng/dome/invoicing/engine/service/TaxService.java
@@ -64,13 +64,13 @@ public class TaxService {
 		return order;
 	}
 
-	public BillingResponseDTO applyTaxes(Product product, CustomerBill cb, List<AppliedCustomerBillingRate> bills)
+	public BillingResponseDTO applyTaxes(CustomerBill cb, List<AppliedCustomerBillingRate> acbrs)
 			throws Exception {
 		
 		List<AppliedCustomerBillingRate> acbrWithTaxes=new ArrayList<AppliedCustomerBillingRate>();
 		
-		for (AppliedCustomerBillingRate bill : bills) {
-			acbrWithTaxes.add(this.applyTaxes(product, bill));
+		for (AppliedCustomerBillingRate acbr : acbrs) {
+			acbrWithTaxes.add(this.applyTaxes(acbr));
 		}
 		
 		this.updateCustomerBillWithTaxes(cb, acbrWithTaxes);
@@ -115,10 +115,10 @@ public class TaxService {
 		return taxItems;
 	}
 
-	private AppliedCustomerBillingRate applyTaxes(Product product, AppliedCustomerBillingRate bill) throws Exception {
+	private AppliedCustomerBillingRate applyTaxes(AppliedCustomerBillingRate bill) throws Exception {
 
 		// retrieve the involved parties
-		List<RelatedParty> involvedParties = this.retrieveRelatedParties(product);
+		List<RelatedParty> involvedParties = this.retrieveRelatedParties(bill);
 		RelatedParty buyer = this.getBuyer(involvedParties);
 		RelatedParty seller = this.getSeller(involvedParties);
 
@@ -185,6 +185,7 @@ public class TaxService {
 		return c;
 	}
 
+	/*
 	private List<RelatedParty> retrieveRelatedParties(Product product) throws Exception {
 		List<it.eng.dome.tmforum.tmf637.v4.model.RelatedParty> parties = product.getRelatedParty();
 		if (parties != null) {
@@ -192,14 +193,37 @@ public class TaxService {
 		}
 		return new ArrayList<RelatedParty>();
 	}
+	*/
 
-	private RelatedParty convert(it.eng.dome.tmforum.tmf637.v4.model.RelatedParty inParty) throws IOException {
+	private List<RelatedParty> retrieveRelatedParties(AppliedCustomerBillingRate acbr) throws Exception {
+		List<it.eng.dome.tmforum.tmf678.v4.model.RelatedParty> parties = acbr.getRelatedParty();
+		if (parties != null) {
+			return this.convert(parties);
+		}
+		return new ArrayList<RelatedParty>();
+	}
+
+//	private RelatedParty convert(it.eng.dome.tmforum.tmf637.v4.model.RelatedParty inParty) throws IOException {
+//		return RelatedParty.fromJson(inParty.toJson());
+//	}
+
+	private RelatedParty convert(it.eng.dome.tmforum.tmf678.v4.model.RelatedParty inParty) throws IOException {
 		return RelatedParty.fromJson(inParty.toJson());
 	}
 
+	/*
 	private List<RelatedParty> convert(List<it.eng.dome.tmforum.tmf637.v4.model.RelatedParty> inList) throws IOException {
 		List<RelatedParty> out = new ArrayList<>();
 		for (it.eng.dome.tmforum.tmf637.v4.model.RelatedParty rp : inList) {
+			out.add(this.convert(rp));
+		}
+		return out;
+	}
+	*/
+
+	private List<RelatedParty> convert(List<it.eng.dome.tmforum.tmf678.v4.model.RelatedParty> inList) throws IOException {
+		List<RelatedParty> out = new ArrayList<>();
+		for (it.eng.dome.tmforum.tmf678.v4.model.RelatedParty rp : inList) {
 			out.add(this.convert(rp));
 		}
 		return out;
