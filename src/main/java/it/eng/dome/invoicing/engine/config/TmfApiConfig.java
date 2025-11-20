7ffd7f50b0744315f916710f3e33163358b921e7
Di Zenzo Carmine Federico <191864161+cdizenzo@users.noreply.github.com>
2025-11-20 12:06:51 +0100

    Enable TMF666 Account Management API integration in configuration

--- src/main/java/it/eng/dome/invoicing/engine/config/TmfApiConfig.java
+++ src/main/java/it/eng/dome/invoicing/engine/config/TmfApiConfig.java
@@ -1,5 +1,6 @@
 package it.eng.dome.invoicing.engine.config;
 
+import it.eng.dome.brokerage.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,11 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import it.eng.dome.brokerage.api.APIPartyApis;
-import it.eng.dome.brokerage.api.AppliedCustomerBillRateApis;
-import it.eng.dome.brokerage.api.CustomerBillApis;
-import it.eng.dome.brokerage.api.ProductCatalogManagementApis;
-import it.eng.dome.brokerage.api.ProductInventoryApis;
 import it.eng.dome.invoicing.engine.tmf.TmfApiFactory;
 
 
@@ -57,6 +53,13 @@ public class TmfApiConfig {
 		logger.info("Initializing of ProductCatalogManagementApis");
 
 		return new ProductCatalogManagementApis(tmfApiFactory.getTMF620ProductCatalogManagementApiClient());
+	}
+
+	@Bean
+	public AccountManagementApis accountManagementApis() {
+		logger.info("Initializing of AccountManagementApis");
+
+		return new AccountManagementApis(tmfApiFactory.getTMF666AccountManagementApiClient());
 	}
 	
 
